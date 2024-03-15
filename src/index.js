20158f1d835c1ce621bffbc12d42a8b55d17ae1a
PLIXYGOAT92 <pierre.moreau2002@gmail.com>
2024-03-15 14:59:47 +0100

    JAVA

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import TestComponent from "./Test"
+
+ReactDOM.render(<TestComponent />, document.getElementById("root"));
